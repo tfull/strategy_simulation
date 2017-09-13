36bf49a4f90444f9f123e6583bb0a730053c2ce1
tfull <tfurukawa.mail@gmail.com>
2017-09-14 00:27:35 +0900

    a

---
+++ js/control.js
@@ -0,0 +1,11 @@
+(function(){
+    window.onload = function(){
+        var canvas = document.getElementById("canvas");
+        var context = canvas.getContext("2d");
+
+        context.fillStyle = "black";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        
+    };
+})();
