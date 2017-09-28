3b402f50dd699a1e4594d388ec0d4f505cf9e9b6
tfull <tfurukawa.mail@gmail.com>
2017-09-29 01:44:27 +0900

    view

--- js/point.js
+++ js/point.js
@@ -3,6 +3,10 @@ function Point(x, y){
     this.y = y;
 }
 
+Point.prototype.equal = function(p){
+    return this.x == p.x && this.y == p.y;
+};
+
 Point.prototype.sub = function(p){
     return new Point(this.x - p.x, this.y - p.y);
 };
--- js/stage.js
+++ js/stage.js
@@ -10,12 +10,14 @@ function Stage(assets){
     view_field.y = 0;
     var view_status = new Group();
     view_status.x = Master.screen_height;
-    this.view = { field: view_field };
+    view_status.y = 0;
+    this.view = { field: view_field, status: view_status };
     this.vertex = new Point(1, 1);
 
     this.cursor = new Point(0, 0);
 
     this.scene.addChild(this.view.field);
+    this.scene.addChild(this.view.status);
 
     this.mode = "field";
 }
@@ -26,30 +28,22 @@ Stage.prototype.load = function(args){
     this.field = field;
     this.player = stage.player;
     this.enemy = stage.enemy;
-    this.cursor = new Point(this.player[0].x, this.player[0].y);
+    this.cursor = new Point(this.player[0].point.x, this.player[0].point.y);
 
     if(field.width < this.block_size){
         this.vertex.x = parseInt((field.width - this.block_size) / 2);
     }else{
-        this.vertex.x = this.player[0].x - parseInt(this.block_size / 2);
+        this.vertex.x = this.player[0].point.x - parseInt(this.block_size / 2);
     }
     if(field.height < this.block_size){
         this.vertex.y = parseInt((field.height - this.block_size) / 2);
     }else{
-        this.vertex.y = this.player[0].y - parseInt(this.block_size / 2);
+        this.vertex.y = this.player[0].point.y - parseInt(this.block_size / 2);
     }
 
-    this.clearField();
-    this.drawField();
-    this.drawUnits();
-    this.drawCursor();
+    this.updateField();
+    this.updateStatus();
 };
-
-Stage.prototype.clearField = function(){
-    while(this.view.field.firstChild){
-        this.view.field.removeChild(this.view.field.firstChild);
-    }
-}
 
 Stage.prototype.drawField = function(){
     for(var i = 0; i < this.block_size; i++){
@@ -70,8 +64,8 @@ Stage.prototype.drawField = function(){
 Stage.prototype.drawUnits = function(){
     for(var i = 0; i < this.player.length; i++){
         var object = this.player[i];
-        var j_cell = object.x - this.vertex.x;
-        var i_cell = object.y - this.vertex.y;
+        var j_cell = object.point.x - this.vertex.x;
+        var i_cell = object.point.y - this.vertex.y;
 
         if(j_cell >= 0 && j_cell < this.block_size && i_cell >= 0 && i_cell < this.block_size){
             var image = object.unit.image;
@@ -83,8 +77,8 @@ Stage.prototype.drawUnits = function(){
 
     for(var i = 0; i < this.enemy.length; i++){
         var object = this.enemy[i];
-        var j_cell = object.x - this.vertex.x;
-        var i_cell = object.y - this.vertex.y;
+        var j_cell = object.point.x - this.vertex.x;
+        var i_cell = object.point.y - this.vertex.y;
 
         if(j_cell >= 0 && j_cell < this.block_size && i_cell >= 0 && i_cell < this.block_size){
             var image = object.unit.image;
@@ -114,10 +108,8 @@ Stage.prototype.keyLeft = function(){
             if(this.cursor.x < this.vertex.x){
                 this.vertex.x = this.cursor.x;
             }
-            this.clearField();
-            this.drawField();
-            this.drawUnits();
-            this.drawCursor();
+            this.updateField();
+            this.updateStatus();
         }
     }
 };
@@ -129,10 +121,8 @@ Stage.prototype.keyRight = function(){
             if(this.cursor.x > this.vertex.x + (this.block_size - 1)){
                 this.vertex.x = this.cursor.x - (this.block_size - 1);
             }
-            this.clearField();
-            this.drawField();
-            this.drawUnits();
-            this.drawCursor();
+            this.updateField();
+            this.updateStatus();
         }
     }
 };
@@ -144,10 +134,8 @@ Stage.prototype.keyUp = function(){
             if(this.cursor.y < this.vertex.y){
                 this.vertex.y = this.cursor.y;
             }
-            this.clearField();
-            this.drawField();
-            this.drawUnits();
-            this.drawCursor();
+            this.updateField();
+            this.updateStatus();
         }
     }
 };
@@ -159,10 +147,75 @@ Stage.prototype.keyDown = function(){
             if(this.cursor.y > this.vertex.y + (this.block_size - 1)){
                 this.vertex.y = this.cursor.y - (this.block_size - 1);
             }
-            this.clearField();
-            this.drawField();
-            this.drawUnits();
-            this.drawCursor();
+            this.updateField();
+            this.updateStatus();
         }
     }
+};
+
+Stage.prototype.drawStatus = function(){
+    var sprite_width = Master.screen_width - Master.screen_height;
+    var sprite_height = parseInt(Master.screen_height / 2);
+    var sprite = new Sprite(sprite_width, sprite_height);
+    sprite.backgroundColor = "rgba(0, 0, 0, 0.2)";
+    this.view.status.addChild(sprite);
+    var unit_object = this.focusedUnitObject();
+    if(unit_object !== null){
+        var unit = unit_object.unit;
+        icon_size = Math.min(parseInt(sprite_width / 4), parseInt(sprite_height / 3));
+        sprite = makeImageSprite(unit.image, icon_size, icon_size, parseInt(sprite_width / 4), 0);
+        sprite.image = getImage(this.assets, unit.image.path);
+        this.view.status.addChild(sprite);
+
+        var top_group = new Group();
+        parameters = [["名前", unit.name], ["兵種", unit.unit_class.name], ["レベル", unit.level], ["体力", unit.hit_point + " / " + unit.max_hit_point]];
+        for(var i = 0; i < parameters.length; i++){
+            top_group.addChild(makeLabel(parameters[i][0] + ": " + parameters[i][1], 0, parseInt(sprite_height / (parameters.length * 3) * i), "#ffffff"));
+        }
+        this.view.status.addChild(top_group);
+
+        var parameter_group = new Group();
+        parameter_group.y = parseInt(sprite_height / 3);
+        parameters = [["　力", unit.power], ["魔力", unit.magic], ["　技", unit.technique], ["速さ", unit.speed], ["幸運", unit.luck], ["守備", unit.defense], ["魔防", unit.resist]];
+        for(var i = 0; i < 7; i++){
+            parameter_group.addChild(makeLabel(parameters[i][0] + ": " + parameters[i][1], 0, parseInt(sprite_height * 2 / (7 * 3) * i), "#ffffff"));
+        }
+        this.view.status.addChild(parameter_group);
+    }
+};
+
+Stage.prototype.updateStatus = function(){
+    this.clearView(this.view.status);
+    this.drawStatus();
+};
+
+Stage.prototype.clearView = function(target){
+    while(target.firstChild){
+        target.removeChild(target.firstChild);
+    }
+};
+
+Stage.prototype.updateField = function(){
+    this.clearView(this.view.field);
+    this.drawField();
+    this.drawUnits();
+    this.drawCursor();
+};
+
+Stage.prototype.focusedUnitObject = function(){
+    var n = this.player.length;
+    for(var i = 0; i < n; i++){
+        var u = this.player[i];
+        if(u.point.equal(this.cursor)){
+            return u;
+        }
+    }
+    n = this.enemy.length;
+    for(var i = 0; i < n; i++){
+        var u = this.enemy[i];
+        if(u.point.equal(this.cursor)){
+            return u;
+        }
+    }
+    return null;
 };
